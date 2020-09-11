05855f90b8f3e6aa49977cb3548d73bcd59af437
Pranav <pstatavarti@gmail.com>
2020-09-11 15:04:14 -0700

    Second commit

--- wordgames.py
+++ wordgames.py
@@ -1,12 +1,19 @@
 word = input("enter a word")
-valid_words = []
-x = 0
-letters_to_numbers = {"a": 1, "b": 2, "c": 3, "d": 4, "e": 5, "f": 6, "g": 7, "h": 8, "i": 9, "j": 10, "k": 11, "l": 12, "m": 13, "n": 14, "o": 15, "p": 16, "q": 17, "r": 18, "s": 19, "t": 20, "u": 21, "v": 22, "w": 23, "x": 24, "y": 25, "z": 26}  
+
 def anagram(word):
+    valid_words = []#same length
+    new_list = []#sorted a-z, equal length
+    final_list = [] #sorted a-z, equal length, same letters
     with open("words.txt", "r") as f:
         for line in f:
-            print(line)
-            x += 1
-            if x > 4:
-                break
+            if len(line.strip()) == len(word):  
+                valid_words.append(line.strip())
+    for i in valid_words:
+        new_list.append(sorted(i))
+    for i in new_list:
+        if i == sorted(word):
+            final_list.append(i)
+    for i in final_list:
+        print(i)
+anagram(word)
         
